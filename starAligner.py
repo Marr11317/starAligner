97a88d7671449776df4e46e50b8636f5ef77d319
Marr11317 <remimarche@gmail.com>
2020-08-11 00:01:37 -0400

    Initial commit. Simply works.

--- starAligner.py
+++ starAligner.py
@@ -11,16 +11,33 @@ print('walk_dir = ' + walk_dir)
 # walk_dir = os.path.abspath(walk_dir)
 print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
-for root, subdirs, files in os.walk(walk_dir):
+for root, subdirs, filenames in os.walk(walk_dir):
     print('--\nroot = ' + root)
 
-    for subdir in subdirs:
-        print('\t- subdirectory ' + subdir)
+    # for subdir in subdirs:
+    #     print('\t- subdirectory ' + subdir)
 
-    for filename in files:
-        if filename[3:] == ".ts":
+    for filename in filenames:
+        if filename.endswith(".ts"):
             file_path = os.path.join(root, filename)
             print('\t- file %s (full path: %s)' % (filename, file_path))
 
-            with open(file_path, 'r+') as f:
-                f_content = f.read()
+            result = ""
+            with open(file_path, 'r') as f:
+                line = f.readline()
+                while (line):
+                    if (line.lstrip().startswith("/**")):
+                        count = line.index("/")
+
+                        result += line
+                        line = f.readline()
+                        while (line.lstrip().startswith("*")):
+                            line = (count + 1) * " " + line.lstrip()
+                            result += line
+                            line = f.readline()
+
+                    result += line
+                    line = f.readline()
+
+            with open(file_path, 'w') as f:
+                f.write(result)
